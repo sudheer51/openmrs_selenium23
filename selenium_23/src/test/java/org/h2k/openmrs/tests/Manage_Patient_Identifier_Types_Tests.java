e80bf8e3797f3cec3a0bb046d12437315454e7ef
sudheer51 <sudheer694@gmail.com>
2023-11-11 07:05:27 +0530

    Update Manage_Patient_Identifier_Types_Tests.java

--- selenium_23/src/test/java/org/h2k/openmrs/tests/Manage_Patient_Identifier_Types_Tests.java
+++ selenium_23/src/test/java/org/h2k/openmrs/tests/Manage_Patient_Identifier_Types_Tests.java
@@ -4,10 +4,10 @@ import org.testng.annotations.Test;
 
 public class Manage_Patient_Identifier_Types_Tests {
 	
-	@Test
+	@Test(description="Validate the patient identifier types")
 	public void validate_add_patient_identifier_types()
 	{
-		
+		System.out.println("Execution of additon tests");
 	}
 	@Test
 	public void validate_delete_patient_identifier_types()
