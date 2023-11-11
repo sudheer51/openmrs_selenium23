0b642381b9d9cd49b717b96154c8ec1ffbb81c67
sudheer <sudheer694@gmail.com>
2023-11-11 06:14:06 +0530

    Adding new tests for Patient IDentifier types

---
+++ selenium_23/src/test/java/org/h2k/openmrs/tests/Manage_Patient_Identifier_Types_Tests.java
@@ -0,0 +1,13 @@
+package org.h2k.openmrs.tests;
+
+import org.testng.annotations.Test;
+
+public class Manage_Patient_Identifier_Types_Tests {
+	
+	@Test
+	public void validate_add_patient_identifier_types()
+	{
+		
+	}
+
+}
